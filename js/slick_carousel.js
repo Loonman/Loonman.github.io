f4aba483b094633314a3c3cf21f11baa1ccb6668
Ryan Huard <ryan.huard@live.ca>
2017-03-12 04:17:55 -0600

    Added slick carousel library and populated projects list.

---
+++ js/slick_carousel.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+      $('.projects').slick({
+          dots: true,
+          autoplay: true,
+          autoplaySpeed: 6000,
+          arrows: false
+      });
+});
